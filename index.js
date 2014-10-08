cd67bbcba08ccfb271268bab1b5748c474e98839
David Corne <davidcorne@gmail.com>
2014-10-08 10:43:17 +0100

    Send a 404 if a file isn't found in index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var fs = require("fs");
 
 app.get("/", function(request, response) {
     response.sendFile(__dirname + "/public/index.html");
 })
 
 app.get("/public/*", function(request, response) {
-    response.sendFile(__dirname + request.path);
+    var fullPath = __dirname + request.path;
+    if (fs.existsSync(fullPath)) {
+        response.sendFile(fullPath);
+    } else {
+        response.status(404).send("File " + request.path + " not found.");
+    }
 })
 
 io.on("connection", function(socket) {
