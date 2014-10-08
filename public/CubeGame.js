d80cf3546ea729c70b57b04d019208a0437a0532
David Corne <davidcorne@gmail.com>
2014-10-08 10:29:43 +0100

    Do some client side development, do proper game loop.

--- public/CubeGame.js
+++ public/CubeGame.js
@@ -1,10 +1,176 @@
+//=============================================================================
+// requestAnimFrame is not well supported so use this shim layer. In reality
+// most modern browsers (including mobile) support it, but Opera Mini doesn't
+// so use this anyway. This is from
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function(callback){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 var CubeGame = {
+    WIDTH: 320,
+    HEIGHT: 480,
+    RATIO: 0,
+    KEYBOARD_MAP: {},
+    current_width: null,
+    current_height: null,
+    scale: 1,
+    offset: {top: 0, left: 0},
+    canvas: null,
+    context: null,
     socket: null,
+    entites: [],
+    playerEntity: null
 };
 
 CubeGame.init = function() {
+    CubeGame.startSocketConnection();
+    CubeGame.initialiseGraphics();    
+    CubeGame.initialiseGame();    
+    
+    CubeGame.loop();
+};
+
+CubeGame.startSocketConnection = function() {
     CubeGame.socket = io();
 };
 
+CubeGame.initialiseGraphics = function() {
+    CubeGame.RATIO = CubeGame.WIDTH / CubeGame.HEIGHT;
+    CubeGame.current_width = CubeGame.WIDTH;
+    CubeGame.current_height = CubeGame.HEIGHT;
+
+    CubeGame.canvas = document.getElementsByTagName("canvas")[0];
+    CubeGame.canvas.width = CubeGame.WIDTH;
+    CubeGame.canvas.height = CubeGame.HEIGHT;
+    
+    CubeGame.context = CubeGame.canvas.getContext("2d");
+
+    CubeGame.resize();
+};
+
+CubeGame.initialiseGame = function() {
+    CubeGame.playerEntity = new CubeGame.PlayerEntity(10, 10);
+    CubeGame.entites.push(CubeGame.playerEntity);
+    
+    // left arrow
+    CubeGame.KEYBOARD_MAP[37] = function() {
+        CubeGame.playerEntity.move({x: -CubeGame.playerEntity.speed, y: 0});
+    };
+    // up arrow
+    CubeGame.KEYBOARD_MAP[38] = function() {
+        CubeGame.playerEntity.move({x: 0, y: -CubeGame.playerEntity.speed});
+    };
+    // right arrow
+    CubeGame.KEYBOARD_MAP[39] = function() {
+        CubeGame.playerEntity.move({x: CubeGame.playerEntity.speed, y: 0});
+    };
+    // down arrow
+    CubeGame.KEYBOARD_MAP[40] = function() {
+        CubeGame.playerEntity.move({x: 0, y: CubeGame.playerEntity.speed});
+    };
+    window.addEventListener("keydown", CubeGame.keyPressed, false);
+};
+
+CubeGame.keyPressed = function(event) {
+    if (CubeGame.KEYBOARD_MAP[event.keyCode] !== undefined) {
+        event.preventDefault();
+        CubeGame.KEYBOARD_MAP[event.keyCode]();
+    }
+};
+
+CubeGame.resize =  function() {
+    CubeGame.current_height = window.innerHeight;
+    // Now keep the correct ratio
+    CubeGame.current_width = CubeGame.current_height * CubeGame.RATIO;
+
+    // Set the canvas style width and height.
+    CubeGame.canvas.style.width = CubeGame.current_width + "px";
+    CubeGame.canvas.style.height = CubeGame.current_height + "px";
+
+    // calculate the offset/scale
+    CubeGame.scale = CubeGame.current_width / CubeGame.WIDTH;
+    CubeGame.offset.top = CubeGame.canvas.offsetTop;
+    CubeGame.offset.left = CubeGame.canvas.offsetLeft;
+};
+
+CubeGame.loop = function() {
+    requestAnimFrame(CubeGame.loop);
+    CubeGame.update();
+    CubeGame.render();
+};
+
+CubeGame.update = function() {
+};
+
+CubeGame.render = function() {
+    CubeGame.Graphics.clear();
+    for (var index = 0; index < CubeGame.entites.length; index += 1) {
+        CubeGame.entites[index].render();
+    }
+    
+};
+
+//=============================================================================
+CubeGame.Graphics = {
+    clear:function() {
+        CubeGame.context.clearRect(0, 0, CubeGame.WIDTH, CubeGame.HEIGHT);
+    },
+
+    rectangle: function(x, y, width, height, colour) {
+        CubeGame.context.fillStyle = colour;
+        CubeGame.context.fillRect(x, y, width, height);
+    },
+
+    circle: function(x, y, radius, colour) {
+        CubeGame.context.fillStyle = colour;
+        CubeGame.context.beginPath();
+        CubeGame.context.arc(x + 5, y + 5, radius, 0, Math.PI * 2, true);
+        CubeGame.context.closePath();
+        CubeGame.context.fill();
+    },
+
+    text: function(message, x, y, size, colour) {
+        CubeGame.context.font = "bold " + size + "px Monospace";
+        CubeGame.context.fillStyle = colour;
+        CubeGame.context.fillText(message, x, y);
+    }
+};
+
+CubeGame.insideScene = function(point) {
+    if (point.x >= 0 && point.x < CubeGame.WIDTH) {
+        if (point.y >= 0 && point.y < CubeGame.HEIGHT) {
+            return true;
+        }
+    }
+    return false;
+};
+
+CubeGame.PlayerEntity = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 5;
+    this.render = function() {
+        CubeGame.Graphics.rectangle(this.x, this.y, 5, 5, "black");
+    };
+    this.move = function(vector) {
+        this.x += vector.x;
+        this.y += vector.y;
+        if (!CubeGame.insideScene({x: this.x, y: this.y})) {
+            this.x -= vector.x;
+            this.y -= vector.y;
+        }
+    };
+};
+
+
 // The false means fire the event at bubbling stage, not capturing.
 window.addEventListener("load", CubeGame.init, false);
+window.addEventListener("resize", CubeGame.resize, false);
